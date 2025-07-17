8c797775ae39c1420db574547ea2f7494a961c29
Anshal-nagvanshi <anshalnagvanshi1177@gmail.com>
2025-07-17 22:03:45 +0530

    Done

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,5 +1,27 @@
+import User from "../models/User.js";
+
 export async function signup (req ,res){
     const {email,password,fullName} = req.body;
+
+    try {
+        if(!email || !password || !fullName){
+            return req.status(400).json({message: "All fields are required"});
+        }
+        if (password.lenght < 6) {
+            return req.status(400).json({message: "Password must be at least 6 characters"});
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+          return res.status(400).json({ message: "Invalid email format" });
+        }
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already exists, please use a diffrent one" });
+        }
+    } catch (error) {
+        
+    }
 }
 export async function login (req ,res){
     res.send("Login Route");
